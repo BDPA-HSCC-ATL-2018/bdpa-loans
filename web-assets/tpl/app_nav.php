929ef79a5309808843fa852504e07d0785a35f40
wilfred66 <wilfredpowelljr@.com>
2018-07-20 23:00:08 -0400

    Display users name in nav

--- web-assets/tpl/app_nav.php
+++ web-assets/tpl/app_nav.php
@@ -1,13 +1,48 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="/bdpa-loans/dashboard.php">Loan Application</a>
-  <a class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" href="/bdpa-loans/index.php?action=logout"></a>
 
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="dashboard.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="references.php">References</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Profile
+        </a>
+  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+    <a class="dropdown-item">
+  <?php
+  $cust_id = $_SESSION['customer_id'];
+$display_sql = <<<SQL
+SELECT * FROM customers WHERE customer_id = $cust_id;
+SQL;
+$display_result = $dbh->query($display_sql);
+
+if ($display_result) {
+while ($row = $display_result->fetch_assoc()) {
+  $display_echo = "
+  Name: " . $row['first_name'] . " " . $row['last_name'] . "
+";
+}
+echo $display_echo;
+}
+
+
+   ?>
+ </a>
+       <a class="dropdown-item"><?php
+         if (isset($_SESSION['customer_id'])) {
+           echo "<a class='btn btn-secondary my-2 my-sm-0' href='/bdpa-loans/index.php?action=logout' style='position: absolute; right: 20px'>Logout</a>";
+         }
+       ?>
+     </a>
+       </div>
     <form class="form-inline my-2 my-lg-0">
-      <?php
-        if (isset($_SESSION['customer_id'])) {
-          echo "<a class='btn btn-secondary my-2 my-sm-0' href='/bdpa-loans/index.php?action=logout' style='position: absolute; right: 20px'>Logout</a>";
-        }
-      ?>
+
     </form>
   </div>
 </nav>
