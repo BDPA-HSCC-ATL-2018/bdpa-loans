05cf9c753819ce84bbe9cf6d48f48d205a530606
wilfred66 <wilfredpowelljr@.com>
2018-07-21 22:38:03 -0400

    Reference View

--- forms/references_view.php
+++ forms/references_view.php
@@ -1,34 +1,15 @@
 <?php
   $page_title = "Reference View";
-  include_once = $_SERVER['DOCUMENT_ROOT'] . '/bdpa-loans/web-assets/tpl/app_header.php';
-  include_once = $_SERVER['DOCUMENT_ROOT'] . '/bdpa-loans/web-assets/tpl/app_nav';
+  include_once $_SERVER['DOCUMENT_ROOT'] . '/bdpa-loans/web-assets/tpl/app_header.php';
+  include_once $_SERVER['DOCUMENT_ROOT'] . '/bdpa-loans/web-assets/tpl/app_nav.php';
   ?>
-  <?php
-  global $dbh;
-    $email = $_SESSION['email'];
-    $cust_id = $_SESSION['customer_id'];
-    $sql = <<<SQL
-      SELECT * FROM loan_application WHERE customer_id = $cust_id;
-SQL;
-
-    $result = $dbh->query($sql);
-
-    if ($result->num_rows > 0) {
-      $new_row = 0;
-      while($row = $result->fetch_assoc()) {
-        if ($new_row % 4 == 0) {
-          echo "<div class='row'>";
-        }
-?>
-
 <div class="col-md-10">
-  <div class="card">
-    <div class="card-header">Reference</div>
-    <div class="card-body">
-      <p class="card-text font-weight-bold">
+
       <?php
+
+      $cust_id = $_SESSION['customer_id'];
         $view_ref_sql = <<<SQL
-          SELECT * FROM customer_reference WHERE reference_id = $cust_id;
+          SELECT * FROM customer_references WHERE customer_id = $cust_id;
 SQL;
         $ref_result = $dbh->query($view_ref_sql);
 
@@ -36,13 +17,20 @@ SQL;
         if ($ref_result) {
           while ($row = $ref_result->fetch_assoc()) {
             $ref_echo = "
+            <div class='card'>
+                <div class='card-header'>Reference</div>
+                <div class='card-body'>
+                  <p class='card-text font-weight-bold'>
             Name: " . $row['ref_first_name'] . " " . $row['ref_last_name'] . "<br><br>
-            Email: " . $row['email_id'] . "<br><br>
             Street Address: " . $row['address_line_one'] . "<br><br>
             City: " . $row['city_name'] . "<br><br>
             State: " . $row['state_cd'] . "<br><br>
             Zip Code: " . $row['postal_cd'] . "<br><br>
+            </div>
+          </div>  
             ";
+            echo $ref_echo;
           }
-          echo $ref_echo;
-          }
+
+        }
+          ?>
